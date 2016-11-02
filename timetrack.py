61056b1298a3024b3a379af2989f10b2a12634a5
Wayne Werner <wayne@waynewerner.com>
2016-11-01 21:27:54 -0500

    Save tasks and report on the day

--- timetrack.py
+++ timetrack.py
@@ -1,8 +1,10 @@
+import re
 import sys
-import time
 import threading
+import time
 
-from datetime import datetime
+from collections import namedtuple, defaultdict
+from datetime import datetime, timedelta
 
 
 # _find_getch came from this answer on stackoverflow.com
@@ -51,11 +53,17 @@ def display(stopper, display_event):
         spent = '{:0>2}:{:0>2}:{:0>2}'.format(hours, minutes, seconds)
 
         if not stopper.isSet():
-            print('\r{} - Choice: {}'.format(spent, text), end='')
+            print('\r{} - Task: {}'.format(spent, text), end='')
 
 
-def main():
-    global text
+def store_task(desc, start, end):
+    with open('timesheet.txt', 'a') as f:
+        fmt = '{:%Y-%m-%d %H:%M:%S} - {:%Y-%m-%d %H:%M:%S}\n\t{}'
+        print(fmt.format(start, end, desc), file=f)
+
+
+def hack_time():
+    global text, start
     stopper = threading.Event()
     display_event = threading.Event()
     display_thread = threading.Thread(
@@ -71,22 +79,26 @@ def main():
             ch = getch()
             if ch == '\r':
                 print()
-                print(text)
-                print()
                 if text == 'q':
                     stopper.set()
                     break
                 else:
+                    store_task(desc=text, start=start, end=datetime.now())
                     text = ''
-            elif ch.isspace() or ch.isalnum():
+                    start = datetime.now()
+            elif ch.isspace() or ch.isprintable():
                 text += ch
             elif ch == '\x7f':
+                spaces = re.sub('[^\t]', ' ', 'xx:xx:xx - Task:'+text)
+                print('\r', spaces, end='')
                 text = text[:-1]
-                print('\r', ' '*len('xx:xx:xx - Task:'+text), end='')
             else:
                 print()
                 print(repr(ch))
-                print()
+        except (KeyboardInterrupt, EOFError) as e:
+            stopper.set()
+            print()
+            break
         except:
             stopper.set()
             raise
@@ -94,8 +106,47 @@ def main():
             display_event.set()
 
     display_thread.join()
-    print('\nBye!')
+
+
+def parse_timespan(timespan):
+    text_start, _, text_end = timespan.partition(' - ')
+    fmt = '%Y-%m-%d %H:%M:%S'
+    start = datetime.strptime(text_start, fmt)
+    end = datetime.strptime(text_end, fmt)
+    return start, end
+
+
+def report_for_date(date):
+    try:
+        Span = namedtuple('Span', 'start, end')
+        tasks = defaultdict(list)
+        with open('timesheet.txt', 'r') as f:
+            for timespan, task in zip(f,f):
+                try:
+                    start, end = parse_timespan(timespan.rstrip())
+                except ValueError:
+                    print('Unable to parse timespan {!r}, task: {!r}'
+                          .format(timespan.strip(), task.strip()))
+                else:
+                    tasks[task.strip()].append(Span(start=start, end=end))
+
+        for task in sorted(tasks):
+            print(task)
+            total = timedelta(0)
+            for span in tasks[task]:
+                total += span.end - span.start
+            print('\t{}'.format(total))
+    except FileNotFoundError:
+        print('Unable to find timesheet.txt, does it exist here?')
+
+
+def main(args):
+    if not args:
+        hack_time()
+    elif args[0] == 'today':
+        report_for_date(datetime.now().date())
+    print('Bye!')
             
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
